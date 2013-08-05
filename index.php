6707fbe3fc7993bd53bd5c322acc03faf8d05608
Mauricio Gelves <mauricio.gelves@tecnilogica.com>
2013-08-05 15:04:00 +0200

    Actualización en maqueta de la Home-Page

--- index.php
+++ index.php
@@ -68,8 +68,151 @@
         	<div class="clear"></div>
         </section><!-- END #what-we-seek -->
         <section id="what-you-will-get">
-	        
+	        <header>
+	        	<span>AFKAR.ME: the perks</span>
+	        	<h2>What we seek</h2>	
+        	</header>
+        	<div class="container_4">
+	        	<ul class="grid_2 alpha">
+		        	<li>
+			        	<span class="step">1</span>
+			        	<p><b>Seed fund</b> for your project so you can focus on your business fulltime</p>
+		        	</li>
+		        	<li>
+			        	<span class="step">2</span>
+			        	<p><b>Someone to guide you</b> at every step of the process</p>
+		        	</li>
+		        	<li>
+			        	<span class="step">3</span>
+			        	<p><b>Office Space</b> to work with us closely on a daily basis</p>
+		        	</li>
+	        	</ul>
+	        	<ul class="grid_2 omega">
+		        	<li>
+			        	<span class="step">4</span>
+			        	<p>All the advice you need from <b>experts in different fields</b> like marketing, business, design, development, legal</p>
+		        	</li>
+		        	<li>
+			        	<span class="step">5</span>
+			        	<p>Access to <b>revenue pipeline</b> and big exposure in our network</p>
+		        	</li>
+		        	<li>
+			        	<span class="step">6</span>
+			        	<p>A <b>fun and creative environment</b> surrounded by very talented people</p>
+		        	</li>
+	        	</ul>
+	        	<div class="clear"></div>
+        	</div><!-- END .container_4 -->
+        	
         </section><!-- END #what-you-will-get -->
+        <section id="the-program">
+	        <header>
+	        	<span>THE PROGRAM: Selection process</span>
+	        	<h2>This is what will happen down the road...</h2>	
+        	</header>
+        	
+        	<img src="img/proceso.png" alt="proceso" width="960" height="90">
+        	
+        	<ul class="container_4 columns">
+	        	<li class="grid_1">
+		        	<header>
+			        	<span>send us your idea</span>
+			        	<h4>Application time</h4>
+		        	</header>
+		        	<div class="description">
+			        	<p>You’ll be able to apply using our form. From all those great ideas submitted we’ll select the 10 that better adapt to our program, which will depend on:</p>
+			        	<ul>
+				        	<li>Which area they are related to</li>
+				        	<li>Their development stage and time  
+   framework</li>
+   							<li>Their commercialization potential</li>
+			        	</ul>
+		        	</div>
+		        	<p>Either selected or not, we’ll let you know by mail</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<header>
+			        	<span>LET US GET TO KNOW YOU BETTER</span>
+			        	<h4>Team Interviews</h4>
+		        	</header>
+		        	<div class="description">
+			        	<p>During the next month we’ll interview each team (via Skype) from the first screening to get to know more about their ideas. We are very interested in knowing what their motivations and expectations are.
+The best five teams from all the interviews will be invited to come to our weekend startup event.</p>
+		        	</div>
+	        	</li>
+	        	<li class="grid_1">
+		        	<header>
+			        	<span>DELIGHT US WITH YOUR PITCH</span>
+			        	<h4>Weekend Event</h4>
+		        	</header>
+		        	<div class="description">
+			        	<p>The top 5 teams will pitch in front of a panel of experts. It’ll be their best opportunity to impress us.<br>We will offer a training course for a weekend so they learn tips to improve not only their pitch but also other relevant points like their business model.</p>
+		        	</div>
+		        	<p>Winners should start the program immediately.</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<header>
+			        	<span>START TO DEVELOP YOUR BUSINESS</span>
+			        	<h4>Welcome!</h4>
+		        	</header>
+		        	<div class="description">
+			        	<p>Congratulations! If your team is here is because you are the ones we fell in love with. We’ll get to work immediately after agreeing which goals you want to achieve during the program.</p>
+		        	</div>
+		        	<p>We can’t wait to start working with you at Afkar!</p>
+	        	</li>
+        	</ul><!-- END .container_4 -->
+        	<div class="clear"></div>
+        	
+        </section><!-- END #the-program -->
+        <section id="our-mentors">
+	        <header>
+	        	<span>NOT ONLY GREAT EXPERTS</span>
+	        	<h2>Our mentors</h2>	
+        	</header>
+        	<ul class="container_4">
+	        	<li class="grid_1">
+		        	<img src="img/mentor-tarek-saadeh.jpg" alt="Tarek Saadeh">
+		        	<p>Tarek Saadeh</p>
+		        	<p>Partner at CMEA Capital</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-omar-hanna.jpg" alt="Omar Hanna">
+		        	<p>Omar Hanna</p>
+		        	<p>Principal at VC/PE firm, Abraaj Capital</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-bhusari.jpg" alt="mentor-bhusari">
+		        	<p>J.C.Bhusari</p>
+		        	<p>VP Engineering at digital rewards startup</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-walid-tabaza.jpg" alt="walid-tabaza">
+		        	<p>Walid Tabaza</p>
+		        	<p>Ceo of Google</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-tarek-saadeh2.jpg" alt="Tarek Saadeh">
+		        	<p>Tarek Saadeh</p>
+		        	<p>Partner at CMEA Capital</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-omar-hanna2.jpg" alt="Omar Hanna">
+		        	<p>Omar Hanna</p>
+		        	<p>Principal at VC/PE firm, Abraaj Capital</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-bhusari2.jpg" alt="mentor-bhusari">
+		        	<p>J.C.Bhusari</p>
+		        	<p>VP Engineering at digital rewards startup</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/mentor-walid-tabaza2.jpg" alt="walid-tabaza">
+		        	<p>Walid Tabaza</p>
+		        	<p>Ceo of Google</p>
+	        	</li>
+
+        	</ul>
+        </section><!-- END #our-mentors -->
         
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
