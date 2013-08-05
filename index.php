ff157a82be15bf06488fc485e44dc50887d19549
Mauricio Gelves <mauricio.gelves@tecnilogica.com>
2013-08-05 13:01:53 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body class="home">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <header id="header">
+        	<div class="container_4">
+	        	<div id="we-support" class="grid_2">
+		        	<img src="img/logo-afkar.png" alt="logo-afkar">
+		        	<p id="entrepreneur">Wanna be an entrepreneur?</p>
+		        	<p id="support">we support</p>
+		        	<p id="digital">digital-oriented startups</p>
+		        	<p id="middle-east">for the Middle East</p>
+		        	<button class="btn">Apply</button>
+		        	<p id="date">Until November 10<sup>th</sup></p>
+	        	</div><!-- END #we-support -->
+	        	<div class="clear"></div>
+        	</div><!-- END .container_4 -->
+        </header>
+        <nav id="navigation">
+	        <ul>
+	        	<li><a href="#">Who we are</a></li>
+	        	<li><a href="#">The program</a></li>
+	        	<li><a href="#">Mentors & Partners</a></li>
+	        	<li><a href="#">FAQ's</a></li>
+	        	<li class="active"><a href="#">Apply <span>Until Nov. 10<sup>th</sup></span></a></li>
+	        </ul>
+        </nav><!-- END #navigation -->
+        <section id="what-we-seek">
+        	<header>
+	        	<span>AFKAR.ME: Applicants requirements</span>
+	        	<h2>What we seek</h2>	
+        	</header>
+        	<ul class="container_4">
+	        	<li class="grid_1">
+		        	<img src="img/icono-jugador.png">
+		        	<p><b>Smart, fun and hard-working teams</b> willing to give their best during 3 months</p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/icono-ciudad.png">
+		        	<p>Availability to relocate to Dubai or Riyadh and <b>work exclusively on your business</b></p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/icono-tablet.png">
+		        	<p><b>Technology-oriented ideas</b> or projects targeting the <b>MENA market</b></p>
+	        	</li>
+	        	<li class="grid_1">
+		        	<img src="img/icono-pez.png">
+		        	<p><b>We value divertisy and originality.</b> If you believe in your idea we do as well</p>
+	        	</li>
+        	</ul>
+        	<div class="clear"></div>
+        </section><!-- END #what-we-seek -->
+        <section id="what-you-will-get">
+	        
+        </section><!-- END #what-you-will-get -->
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
